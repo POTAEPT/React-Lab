faebd72f3b2fcd92b2a44fead982dcaf5d9fbf49
Thitirat Sirisawad <thitirat_sirisawad@cmu.ac.th>
2026-09-13 14:47:54 +0700

    Add FilterBar component and integrate it into App for user filtering

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,1 +1,38 @@
 // ว่างไว้ตั้งใจ — เขียนเองใน Lab A (13:00–13:55)
+
+
+function FilterBar({ query, userFollowers, handleQueryChange, handleUserFollowersChange }) {
+
+    const textFilter = "(login หรือ name)"
+
+
+    return (
+        <div>
+            <div className="filter-bar">
+                <p>ค้นหา {textFilter}</p>
+                <input
+                    className="border rounded"
+                    type='text'
+                    value={query}
+                    onChange={(e) => {
+                        handleQueryChange(e.target.value)
+                    }} placeholder='Search' />
+            </div>
+            <div>
+                <p>ผู้ติดตามขั้นต่ำ</p>
+                <input
+                    className="border rounded"
+                    type='number'
+                    value={userFollowers}
+                    onChange={(e) => {
+                        handleUserFollowersChange(Number(e.target.value))
+                    }} placeholder="userFollowers" />
+            </div>
+        </div>
+    )
+
+}
+
+
+
+export default FilterBar;
