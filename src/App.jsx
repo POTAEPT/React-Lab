fc28d38f8510a66b0d4c3f8f484e55417c1a0da4
Potae <n.natthawutpt@gmail.com>
2026-09-19 13:39:06 +0700

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,36 @@
 // hooks/useFetch.js มีให้แล้ว (ของวันที่ 3) — ใช้ต่อได้เลย ไม่ต้องเขียนใหม่
 
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout"
-import Home from "./pages/Home"
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
+import RecipeDetail from "./pages/RecipeDetail";
 import About from "./pages/Home";
-import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound";
+import { Suspense } from "react";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element = {<Layout />}>
-        <Route index element={<Home />}/>
-        <Route path="/recipes" element={<Recipes />}/>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route
+            path="/recipes/:id"
+            element={
+              <Suspense fallback={<p>...กำลังโหลด...</p>}>
+                <RecipeDetail />
+              </Suspense>
+            }
+          />
 
-        <Route path="/about" element={<About />}/>
-        <Route path="*" element={<NotFound/>}/>
-
-
-      </Route>
-    </Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
